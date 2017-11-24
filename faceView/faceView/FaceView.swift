b2f20fc3aba8aff49bee6b9dfe4dc0f80f15d557
osaOgbebor <oogbe001@fiu.edu>
2017-11-23 19:28:36 -0800

    IBInspectable & IBDesginable added

--- faceView/faceView/FaceView.swift
+++ faceView/faceView/FaceView.swift
@@ -8,11 +8,26 @@
 
 import UIKit
 
+@IBDesignable
 class FaceView: UIView {
     
+    
+    @IBInspectable
     var scale: CGFloat = 0.9
     
+    @IBInspectable
     var eyesOpen: Bool = true
+    
+    @IBInspectable
+    var lineWidth: CGFloat = 5.0
+    
+    @IBInspectable
+    var color: UIColor = UIColor.blue
+    
+    
+    @IBInspectable
+    var mouthCurvature: Double = -0.5 // 1.0 is full smile and -1.0 is full frown
+    
     
     private var skullRadius: CGFloat      {
         return min(bounds.size.width, bounds.size.height) / 2 * scale
@@ -53,7 +68,7 @@ class FaceView: UIView {
             
         }
             
-        path.lineWidth = 5
+        path.lineWidth = lineWidth
         
         return path
         
@@ -72,7 +87,21 @@ class FaceView: UIView {
             height: mouthHeight
         )
         
-        let path = UIBezierPath(rect: mouthRect)
+        let smileOffset = CGFloat(max(-1, min(mouthCurvature, 1))) * mouthRect.height
+        
+        let start = CGPoint(x: mouthRect.minX, y: mouthRect.midY)
+        let end = CGPoint(x: mouthRect.maxX, y: mouthRect.midY)
+        
+        let cp1 = CGPoint(x: start.x + mouthRect.width / 3, y: start.y + smileOffset)
+        let cp2 = CGPoint(x: end.x - mouthRect.width / 3, y: start.y + smileOffset)
+        
+        
+        let path = UIBezierPath()
+        path.move(to: start)
+        path.addCurve(to: end, controlPoint1: cp1, controlPoint2: cp2)
+        
+        path.lineWidth = lineWidth
+        
         return path
     }
     
@@ -81,7 +110,7 @@ class FaceView: UIView {
     // Creating path for skull
     private func pathForSkull() -> UIBezierPath {
         let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false)
-        path.lineWidth = 5.0
+        path.lineWidth = lineWidth
         return path
     }
     
@@ -90,7 +119,7 @@ class FaceView: UIView {
         // Drawing code
     
         
-        UIColor.blue.set()
+        color.set()
         pathForSkull().stroke()
         
         // calling the pathForEye method, taking its return value and immiediately sending it a message
