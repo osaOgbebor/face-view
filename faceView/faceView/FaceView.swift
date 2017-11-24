97dc497fba0083617dbaa6f3bfe4ad1aa152ac3a
osaOgbebor <oogbe001@fiu.edu>
2017-11-23 16:55:08 -0800

    drawn eye path

--- faceView/faceView/FaceView.swift
+++ faceView/faceView/FaceView.swift
@@ -20,6 +20,35 @@ class FaceView: UIView {
         return CGPoint(x: bounds.midX, y: bounds.midY)
     }
     
+    // creating path for eye
+    private enum Eye    {
+        case left
+        case right
+    }
+    
+    
+    private func pathForEye (_ eye: Eye) -> UIBezierPath   {
+        
+        func centerOfEye(_ eye: Eye) -> CGPoint     {
+            let eyeOffset = skullRadius / Ratios.skullRadiusToEyeOffset
+            var eyeCenter = skullCenter
+            eyeCenter.y -= eyeOffset
+            eyeCenter.x += ((eye == .left) ? -1 : 1) * eyeOffset
+            return eyeCenter
+        }
+        
+        let eyeRadius = skullRadius / Ratios.skullRadiusToEyeRadius
+        let eyeCenter = centerOfEye(eye)
+        
+        let path = UIBezierPath(arcCenter: eyeCenter, radius: eyeRadius, startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: true)
+        path.lineWidth = 5
+        
+        return path
+        
+    }
+    
+    
+    // Creating path for skull
     private func pathForSkull() -> UIBezierPath {
         let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false)
         path.lineWidth = 5.0
@@ -33,7 +62,19 @@ class FaceView: UIView {
         
         UIColor.blue.set()
         pathForSkull().stroke()
+        pathForEye(.left).stroke()
+        pathForEye(.right).stroke()
+        
     }
     
+    
+    private struct Ratios   {
+        static let skullRadiusToEyeOffset: CGFloat = 3
+        static let skullRadiusToEyeRadius: CGFloat = 10
+        static let skullRadiusToMouthWidth: CGFloat = 1
+        static let skullRadiusToMouthHeight: CGFloat = 3
+        static let skullRadiusToMouthOffset: CGFloat = 3
+        
+    }
     
 }
