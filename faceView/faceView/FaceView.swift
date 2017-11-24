4934db07d2d697b510cdb07a0933e492a17d79ad
osaOgbebor <oogbe001@fiu.edu>
2017-11-23 16:23:02 -0800

    face radius and center now available to all methods

--- faceView/faceView/FaceView.swift
+++ faceView/faceView/FaceView.swift
@@ -10,16 +10,29 @@ import UIKit
 
 class FaceView: UIView {
     
+    var scale: CGFloat = 0.9
+    
+    private var skullRadius: CGFloat      {
+        return min(bounds.size.width, bounds.size.height) / 2 * scale
+    }
+    
+    private var skullCenter: CGPoint    {
+        return CGPoint(x: bounds.midX, y: bounds.midY)
+    }
+    
+    private func pathForSkull() -> UIBezierPath {
+        let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false)
+        path.lineWidth = 5.0
+        return path
+    }
+    
     
     override func draw(_ rect: CGRect) {
         // Drawing code
+    
         
-        let skullRadius = min(bounds.size.width, bounds.size.height) / 2
-        let skullCenter = CGPoint(x: bounds.midX, y: bounds.midY)
-        let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false)
-        path.lineWidth = 5.0
         UIColor.blue.set()
-        path.stroke()
+        pathForSkull().stroke()
     }
     
     
